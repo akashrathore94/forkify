c89237a9be06acd08f241f1acfbed6ff2b73e223
akashrathore94 <akashrathour2011@gmail.com>
2024-12-04 13:41:39 +0530

    publisher/subscriber pattern added

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,21 +3,14 @@ import recipeView from './views/recipeView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
 
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
 
@@ -30,12 +23,12 @@ const showRecipe = async function () {
     // 2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-// showRecipe();
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
+init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,27 +1,37 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-  const res = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e880a'
-  );
-  const data = await res.json();
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
 
-  if (!res.ok) throw new Error(`${data.message} ${res.status}`);
-
-  let { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    publisher: recipe.publisher,
-    sourceUrl: recipe.source_url,
-    image: recipe.image_url,
-    servings: recipe.servings,
-    cookingTime: recipe.cooking_time,
-    ingredients: recipe.ingredients,
-  };
-
-  console.log(state.recipe, 'from model before await');
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    throw err;
+  }
 };
+
+export const loadSearhResults = async function (query) {
+  try {
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log('data', data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+loadSearhResults('pizza');
