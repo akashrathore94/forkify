5d13528944eb7e4412d057332eebc269d9f9f5dd
akashrathore94 <akashrathour2011@gmail.com>
2024-12-08 17:42:20 +0530

    addRecipeView initiated

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
 import bookmarksView from './views/bookmarksView';
+import addRecipeView from './views/addRecipeView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -90,6 +91,10 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -97,6 +102,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,7 +1,6 @@
 import View from './View';
 import previewView from './previewView';
 import icons from 'url:../../img/icons.svg';
-import previewView from './previewView';
 
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
